e69636ff024afddc08fe93c43295fd542d1c3b58
changamanda <amanda.chang10@gmail.com>
2015-06-02 14:55:52 -0400

    More user functionality

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def image_if_image
+    image_tag @user.image, width: 200 if @user.image.url
+  end
+
+  def edit_image_if_current_user
+    link_to("Edit Photo", edit_photo_path(@user.id), class: "btn btn-default btn-xs", id: "photo-button") if @user == current_user
+  end
 end
